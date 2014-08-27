d96f9d08e71243c0faba9cf8eee89fe8c8c9a852
zmywly8866 <zmywly8866@163.com>
2014-08-27 11:27:26 +0800

    改善加载大图导致OOM的问题。
    改善加载大图导致OOM的问题。

--- src/com/uperone/generateheader/GenerateHeaderActivity.java
+++ src/com/uperone/generateheader/GenerateHeaderActivity.java
@@ -11,6 +11,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
+import android.graphics.Rect;
 import android.net.Uri;
 import android.os.Environment;
 import android.text.Editable;
@@ -141,22 +142,55 @@ public class GenerateHeaderActivity extends BaseActivity {
 		Uri uri = data.getData();  
         Log.e("uri", uri.toString());  
         ContentResolver cr = this.getContentResolver();  
-        try {  
-        	mBitmap = BitmapFactory.decodeStream(cr.openInputStream(uri));
-        	float height = getResources( ).getDimension( R.dimen.icon_width );
-        	float scale = (mBitmap.getHeight( )*1.0f)/height;
-        	
-        	mBitmap = getScaleBitmap( mBitmap, scale );
-            mHeaderImg.setImageBitmap( mBitmap );
-            
-            if( null == mBadgeView ){
-            	mBadgeView = new BadgeView(this, mHeaderImg);
-            }
-            mBadgeView.setText( getShowIndex( ) );
-            mBadgeView.show();
-        } catch (FileNotFoundException e) {  
-            Log.e("Exception", e.getMessage(),e);  
-        }  
+
+        int height = ( int )( getResources( ).getDimension( R.dimen.icon_width ) );
+    	mBitmap = decodeSampledBitmapFromResource(cr, uri, height, height );
+        mHeaderImg.setImageBitmap( mBitmap );
+        
+        if( null == mBadgeView ){
+        	mBadgeView = new BadgeView(this, mHeaderImg);
+        }
+        mBadgeView.setText( getShowIndex( ) );
+        mBadgeView.show(); 
+	}
+	
+	private Bitmap decodeSampledBitmapFromResource(ContentResolver cr, Uri uri, int reqWidth, int reqHeight) {
+		// 第一次解析将inJustDecodeBounds设置为true，来获取图片大小
+	    final BitmapFactory.Options options = new BitmapFactory.Options();
+	    options.inJustDecodeBounds = true;
+	    try {
+			BitmapFactory.decodeStream(cr.openInputStream(uri), new Rect(0,0,0,0), options);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	    // 调用上面定义的方法计算inSampleSize值
+	    options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+	    // 使用获取到的inSampleSize值再次解析图片
+	    options.inJustDecodeBounds = false;
+	    try {
+			return BitmapFactory.decodeStream(cr.openInputStream(uri), new Rect(0,0,0,0), options);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	    
+	    return null;
+	}
+	
+	private int calculateInSampleSize(BitmapFactory.Options options,
+			int reqWidth, int reqHeight) {
+		// 源图片的高度和宽度
+		final int height = options.outHeight;
+		final int width = options.outWidth;
+		int inSampleSize = 1;
+		if (height > reqHeight || width > reqWidth) {
+			// 计算出实际宽高和目标宽高的比率
+			final int heightRatio = Math.round((float) height / (float) reqHeight);
+			final int widthRatio = Math.round((float) width / (float) reqWidth);
+			// 选择宽和高中最小的比率作为inSampleSize的值，这样可以保证最终图片的宽和高
+			// 一定都会大于等于目标的宽和高。
+			inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
+		}
+		return inSampleSize;
 	}
 	
 	private String getShowIndex( ){
